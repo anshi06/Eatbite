58ccf3504866afc0f2721a7cc70d7849b0cf58d0
anshi06 <aradhya6768@gmail.com>
2024-05-03 11:56:59 +0530

    indexjs is serving now

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,9 @@
 const { app } = require(".");
 const connectToDB = require("./config/db");
 
-connectToDB();
-
 // const PORT=5454;
 // app.listen(PORT,async ()=>{
 //     await connectToDB()
 //     console.log("food ordering server running on port ",PORT)
 // })
 
-module.exports = app;
