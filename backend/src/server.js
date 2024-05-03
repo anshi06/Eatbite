5e449ef9f24c301808156ab1ea2077e1c37a2236
anshi06 <aradhya6768@gmail.com>
2024-05-03 11:52:00 +0530

    export app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const { app } = require(".");
 const connectToDB = require("./config/db");
 
-const PORT=5454;
-app.listen(PORT,async ()=>{
-    await connectToDB()
-    console.log("food ordering server running on port ",PORT)
-})
+connectToDB();
 
+// const PORT=5454;
+// app.listen(PORT,async ()=>{
+//     await connectToDB()
+//     console.log("food ordering server running on port ",PORT)
+// })
+
+module.exports = app;
